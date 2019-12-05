ae528296bc0b110c83bf3a2762dad68e2f85b0cc
Jonte Williams <jontedesign@Jontes-MacBook-Pro.local>
2019-12-04 18:49:36 -0800

    Alling all of the projects I've done so far

---
+++ Jonte/Day7/real.js
@@ -0,0 +1,8 @@
+const h1 = document.createElement("h1");
+document.body.appendChild(h1);
+h1.innerHTML = "Our Project";
+
+// paragraph that explains the app
+const p = document.createElement("p");
+document.body.appendChild(p);
+p.innerHTML = "This is an app made with js";
